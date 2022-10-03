bfe69ce5885b1b4a00bf77e78d1709d1fcd34f61
Amanda Natallie <amanda@vallorisolutions.com>
2022-10-03 13:58:34 -0300

    chore(core): upgrade to React 18 + fix PWA build

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { CookiesProvider } from 'react-cookie'
-import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { store } from 'store'
@@ -8,7 +7,11 @@ import App from './App'
 import reportWebVitals from './reportWebVitals'
 import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 
-ReactDOM.render(
+import { createRoot } from 'react-dom/client'
+const container = document.getElementById('root') as HTMLElement
+const root = createRoot(container)
+
+root.render(
   <StrictMode>
     <Provider store={store}>
       <Router>
@@ -18,12 +21,11 @@ ReactDOM.render(
       </Router>
     </Provider>
   </StrictMode>,
-  document.getElementById('root'),
 )
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister()
+serviceWorkerRegistration.register()
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
